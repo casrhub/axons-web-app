5499581f66038e10fff4be7ab938291cc20fcf0b
César Silva <casrr@proton.me>
2024-03-15 12:26:24 -0600

    added documentation route

---
+++ src/api-documentation/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+import { useEffect, useState } from 'react';
+
+export default function ApiDocs() {
+  const [swaggerSpec, setSwaggerSpec] = useState(null);
+
+  useEffect(() => {
+    fetch('/swagger.json')
+      .then((response) => response.json())
+      .then((json) => setSwaggerSpec(json));
+  }, []);
+
+  return swaggerSpec ? <SwaggerUI spec={swaggerSpec} /> : <p>Loading...</p>;
+}
+
+
