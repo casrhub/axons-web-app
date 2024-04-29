0bd7f3600aebf7f3cd3473534d82465841316a69
Your Name <A01254679@tec.mx>
2024-04-29 12:24:35 -0600

    Still working on feedback feature part 2

--- src/app/contact_us/page.jsx
+++ src/app/contact_us/page.jsx
@@ -17,11 +17,11 @@ export default function page() {
           <div className="container flex items-center justify-center flex-col rounded-2xl w-1/3 h-full bg-white shadow-xl mx-2 mt-4">
               <p className="text-2xl text-center font-bold m-3">Support (LinkedIn):<br/></p>
               <p>
-                <a className="text-base text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/cesaralansilva/">Omar Michel Carmona Villalobos</a><br/> 
-                <a className="text-base text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/cesaralansilva/">Miguel Ángel Barrientos Ballesteros</a><br/>
-                <a className="text-base text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/cesaralansilva/">Cesar Alán Silva Ramos</a><br/>
-                <a className="text-base text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/juli%C3%A1n-enrique-espinoza-valenzuela-a145a2287/">Julian Enrique Espinoza Valenzuela</a><br/>
-                <a className="text-base text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/cesaralansilva/">Gabriel Alvarez Arzate</a></p>  
+                <a className="text-2xl text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/cesaralansilva/">Omar Michel Carmona Villalobos</a><br/> 
+                <a className="text-2xl text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/cesaralansilva/">Miguel Ángel Barrientos Ballesteros</a><br/>
+                <a className="text-2xl text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/cesaralansilva/">Cesar Alán Silva Ramos</a><br/>
+                <a className="text-2xl text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/juli%C3%A1n-enrique-espinoza-valenzuela-a145a2287/">Julian Enrique Espinoza Valenzuela</a><br/>
+                <a className="text-2xl text-center font-bold m-3 text-blue-900 hover:text-blue-700" href = "https://www.linkedin.com/in/cesaralansilva/">Gabriel Alvarez Arzate</a></p>  
           </div>
           {/* White box */}
           <div className="container flex items-center justify-center flex-col rounded-2xl w-1/3 h-full bg-white shadow-xl mx-2 mt-4">
@@ -30,7 +30,7 @@ export default function page() {
           {/* White box */}
           <div className="container flex items-center justify-center flex-col rounded-2xl w-1/3 h-full bg-white shadow-xl mx-2 mt-4">
             <p className="text-2xl text-center font-bold m-3">Team Members:<br/></p>
-            <p className="text-base text-center font-bold m-3">Omar Michel Carmona Villalobos |<span className = "text-blue-900"> A01644146</span><br/> 
+            <p className="text-2xl text-center font-bold m-3">Omar Michel Carmona Villalobos |<span className = "text-blue-900"> A01644146</span><br/> 
             Miguel Ángel Barrientos Ballesteros |<span className = "text-blue-900"> A01637150</span><br/>
             Cesar Alán Silva Ramos |<span className = "text-blue-900"> A01252916</span><br/>
             Julian Enrique Espinoza Valenzuela |<span className = "text-blue-900"> A01254679</span><br/>
